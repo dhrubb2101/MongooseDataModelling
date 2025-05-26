e8ecd9bb79aa2acd8b75203e01b653c2b3dcb7c1
dhrubb <dhrubbgupta@gmail.com>
2025-05-26 14:57:49 +0530

    defined the subtodo schema

--- Models/todos/sub_todo.models.js
+++ Models/todos/sub_todo.models.js
@@ -1,6 +1,19 @@
 import mongoose from 'mongoose';
  
-const subTodoSchema = new mongoose.Schema({},{timestamps:true});
+const subTodoSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true,
+    },
+    complete:{
+        type:Boolean,
+        default:false, //if not given then it will be false
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId, //this is the id of the user who created this subTodo
+        ref:"User" //this is the reference to the User model, which means this field will store the ObjectId of a User document
+    }
+},{timestamps:true});
 
 export const SubTodo = mongoose.model("SubTodo",subTodoSchema);
 
