6eded8470cacc7bd78aa7c7c4d945f8c1cd72183
dhrubb <dhrubbgupta@gmail.com>
2025-05-26 14:39:40 +0530

    one change it was the userSchema we did build earlier and then the todo schema and now the subtodo schema in mongoose

--- Models/todos/todo.models.js
+++ Models/todos/todo.models.js
@@ -1,3 +1,8 @@
 import mongoose from 'mongoose';
 
-const todoSchema = new mongoose.Schema({},{timestamps: true});
+const todoSchema = new mongoose.Schema({},{timestamps: true}); //schema is a method in which we enter objects
+
+export const Todo = mongoose.model("Todo",todoSchema)
+//in the parameters first one we enter the name of mongodb database we are adding 
+// and second is kiske basis pr schema banau
+//when this Todo named database gets stored in mongodb what will be it's name it will be todos
