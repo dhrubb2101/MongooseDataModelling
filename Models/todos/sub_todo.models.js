368036b18f292c6ce84cab12815f4a71ccf222b5
dhrubb <dhrubbgupta@gmail.com>
2025-05-26 14:12:25 +0530

    basically created sub_todo mongoose schema which involves entering an object which defines fields in it like email, password and what not and a secondary object which enters the timestamp

--- Models/todos/sub_todo.models.js
+++ Models/todos/sub_todo.models.js
@@ -1,6 +1,29 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema(
+    {
+        // username:String,
+        // email:String,
+        // isActive:Boolean
+        //another way to define a field is like this:
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: Boolean,
+            required: [true, "Password is required"]
+        }
+    },{timestamps: true} // This option adds createdAt and updatedAt timestamps to the schema
+)
 
 export const User = mongoose.model("User", userSchema);             
 //explain this code
@@ -10,3 +33,11 @@ export const User = mongoose.model("User", userSchema);
 //model is a method that creates a model based on the schema.
 //First what model should be created is passed as the first parameter, and the second parameter is the name of the model. that is on who should it be based
 //we will always write this three code lines in the beginning of the file
+
+//we use export const User because we want to use this model/schema in other files
+//we use mongoose to create a model based on the schema. and when it is connected to database, these files automatically run and
+//create a collection in the database with the name of the model, which is "User" in this case and will be based on userSchema.
+// The `User` model can now be used to interact with the "users" collection in the MongoDB database.
+
+//the database name is visible as users in the MongoDB Compass, which is the default name for the collection created by Mongoose when the model is named "User".
+
